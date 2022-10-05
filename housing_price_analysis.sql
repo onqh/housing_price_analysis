9a2bbf522f6994920ab82ed4bfdaf469ebd9ac96
Daniel Huy Nguyen <60880546+onqh@users.noreply.github.com>
2022-10-05 16:11:07 +0100

    Update housing_price_analysis.sql

--- housing_price_analysis.sql
+++ housing_price_analysis.sql
@@ -53,60 +53,141 @@ WITH cte AS
 FROM houses)
 SELECT *
 FROM cte
-WHERE row_number = 1;
+WHERE row_number > 1;
 
--- SELECT *
--- FROM cte
--- WHERE tid = '{453D27A3-ECD6-EF91-E050-A8C0630574D7}'
--- ORDER BY tid;
-
--- FIND INACCURATE PRICE
+-- FIND OUTLIER PRICE: TODO: DEALING WITH OUTLIERS by percentile or mean
 SELECT *
 FROM houses
-WHERE sold_price < 10000 OR sold_price > 10000000;
+WHERE sold_price < 1000 OR sold_price > 50 * 10^6
+ORDER BY sold_price DESC;
 
 ----------------------------------------------------------------------
 -- TID: Transaction ID: unique ID for each time a property is sold
 
 -- PROPERTY TYPE: D = Detachted, S = Semi-Detached, T = Terraced, F = Flat, O = Others
--- to find NULL
-SELECT COUNT(DISTINCT property_type) AS "Number of property type"
-FROM houses;
-
 -- count each types
-SELECT property_type, COUNT(*) 
+SELECT h2.year, h2.property_type, h2.volume, h2.average_price
+FROM
+(SELECT extract(year from sold_date) AS year, property_type, COUNT(*) AS volume, round(AVG(sold_price), 2) AS Average_price
+FROM houses
+GROUP BY 1,2) h2
+ORDER BY 1,2;
+
+-- average price each types
+SELECT property_type,COUNT(*), round(AVG(sold_price), 2) AS average_price
 FROM houses
 GROUP BY property_type
-ORDER BY 2;
+ORDER BY 2 DESC;
 
 -- NEW BUILD
-SELECT new_build, COUNT(*)
+-- count total new build
+SELECT new_build, COUNT(*), round(AVG(sold_price), 2) AS average_price
 FROM houses
 GROUP BY new_build
 ORDER BY 2;
 
--- New Build vs Property type
-SELECT property_type, new_build, COUNT(*)
+-- each type and each year
+SELECT h2.year, h2.new_build, h2.volume, h2.average_price
+FROM
+(SELECT extract(year from sold_date) AS year, new_build, COUNT(*) AS volume, round(AVG(sold_price), 2) AS Average_price
 FROM houses
-GROUP BY 1,2;
+GROUP BY 1,2) h2
+ORDER BY 1,2;
+
+-- New Build vs Property type
+SELECT property_type, new_build, COUNT(*), round(AVG(sold_price), 2) AS average_price
+FROM houses
+GROUP BY 1,2
+ORDER BY 1,2 DESC;
 
 -- DURATION: Leashold and Freehold or Unknown
-SELECT duration, COUNT(*)
+SELECT duration, COUNT(*), round(AVG(sold_price), 2) AS average_price
 FROM houses
 GROUP BY 1;
 
--- TOWN/CITY
-SELECT town, COUNT(*)
+SELECT h2.year, h2.duration, h2.volume, h2.average_price
+FROM
+(SELECT extract(year from sold_date) AS year, duration, COUNT(*) AS volume, round(AVG(sold_price), 2) AS Average_price
 FROM houses
-GROUP BY 1
-ORDER BY 2;
+GROUP BY 1,2) h2
+ORDER BY 1,2;
 
--- PRICE: 
--- Average Price per year
-SELECT extract(year from sold_date) AS Year, AVG(sold_price) AS "Average Price"
+-- TOWN/CITY
+-- SELECT town, COUNT(*) AS frequency, round(AVG(sold_price), 2) AS average_price, AVG(COUNT(*)) OVER () AS mean
+-- FROM houses
+-- GROUP BY 1
+-- ORDER BY 3 DESC;
+
+-- Only take the town with number of property sold large than the average
+SELECT x.town,x.volume, round(x.average_price, 2) AS Average_price
+FROM (
+	SELECT h.town, AVG(h.sold_price) AS average_price, COUNT(h.*) AS volume, AVG(COUNT(h.*)) OVER () AS mean
+	FROM houses AS h
+	GROUP BY 1
+	ORDER BY 2 DESC
+) x
+WHERE x.volume > x.mean
+ORDER BY 3 DESC;
+
+
+-- -- Count by year for each town
+-- SELECT extract(year from sold_date) AS year, town, COUNT(*) AS volume
+-- FROM houses
+-- GROUP BY 1, 2
+-- ORDER BY 3 DESC
+
+-- COUNTY
+SELECT county, round(AVG(sold_price),2) AS average_price, COUNT(*) AS volume
+FROM houses 
+GROUP BY 1
+ORDER BY 3 DESC
+
+-- QUUESTION: 
+-- Average Price per year and  Year on Year Price changes
+WITH year_price AS
+(SELECT extract(year from sold_date) AS year, 
+ 		round(AVG(sold_price),2) AS average_price
 FROM houses
 GROUP BY 1
+)
+SELECT  year AS current_year, 
+ 		average_price AS current_average_price,
+ 		round(100* (average_price - LAG(average_price) OVER(ORDER BY year)) / LAG(average_price) OVER(ORDER BY year) , 2) AS YoY_changes
+FROM year_price
+GROUP BY 1, 2
 ORDER BY 1;
 
+-- Sales volume and YoY change
+SELECT h.year AS current_year,
+	   h.sales_volume AS sales_volume,
+	   round(100 * (h.sales_volume - LAG(h.sales_volume) OVER(ORDER BY h.year)) / LAG(h.sales_volume) OVER(ORDER BY h.year),2) AS YoT_changes
+FROM
+(
+	SELECT extract(year from h.sold_date) AS year,
+	   COUNT(h.*) AS sales_volume
+	FROM houses AS h
+	GROUP BY 1
+) h
+GROUP BY 1, 2
+ORDER BY 1
+
+-- Sales distribution
+SELECT dist_group, COUNT(*)
+FROM
+(
+ SELECT CASE WHEN sold_price BETWEEN 0 AND 99999 THEN '(0, 99999)'
+             WHEN sold_price BETWEEN 100000 AND 199999 THEN '(100000, 199999)'
+             WHEN sold_price BETWEEN 200000 AND 299999 THEN '(200000, 299999)'
+			 WHEN sold_price BETWEEN 300000 AND 399999 THEN '(300000, 399999)'
+	         WHEN sold_price BETWEEN 400000 AND 499999 THEN '(400000, 499999)'
+             WHEN sold_price BETWEEN 500000 AND 599999 THEN '(500000, 599999)'
+			 WHEN sold_price BETWEEN 600000 AND 699999 THEN '(600000, 699999)'
+	         WHEN sold_price BETWEEN 700000 AND 799999 THEN '(700000, 799999)'
+             WHEN sold_price BETWEEN 800000 AND 899999 THEN '(800000, 899999)'
+			 WHEN sold_price BETWEEN 900000 AND 999999 THEN '(900000, 999999)'
+	         ELSE '(10000000+)' AS dist_group
+ FROM HOUSES
+)
+GROUP BY dist_group
 
 
